76ee1d3c5cfb21c20d1b873912bbfa93ddb8ea53
Evelyn Veiled <eveiled@pr-d3.kzn.21-school.ru>
2022-06-25 22:19:34 +0300

    first commit

---
+++ gRPC-client/src/main/java/org/example/Client.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext().build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub =
+                GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setName("Evgenii").setHobbies("programming").build();
+
+        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+
+        System.out.println(response);
+
+        channel.shutdownNow();
+    }
+}
